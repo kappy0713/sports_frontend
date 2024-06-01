8947349ab8d6122e84030dcf06b2da8bef5cf765
kappy0713 <s22253@g.ashikaga.ac.jp>
2024-06-02 00:26:49 +0900

    feature：CORS

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,25 @@
-import Image from "next/image";
+"use client";
+
+import React, { useEffect, useState } from 'react';
 
 export default function Home() {
+  const [data, setData] = useState('');
+
+  const URL = process.env.SERVER_URL;
+  if (!URL) {
+    throw new Error('SERVER_URL is not defined');
+  }
+  useEffect(() => {
+    fetch(URL)
+      .then(response => response.text())
+      .then(data => setData(data))
+      .catch(error => console.error('Error:', error));
+  }, []);
+
   return (
     <div>
-      <h1>Home</h1>
+      <h1>Response from Go Server</h1>
+      <p>{data}</p>
     </div>
   );
 }
