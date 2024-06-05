49146fbeaff9936b2193fe8e9f059efe37f4d95e
kappy0713 <s22253@g.ashikaga.ac.jp>
2024-06-06 08:57:50 +0900

    feature：ログアウト

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,6 +33,11 @@ export default function Home() {
     }
   }, []);
 
+  const Logout = () => {
+    document.cookie = "token=; max-age=0";
+    router.push("/login")
+  }
+
   return (
     <div>
       <h1>Hello World</h1>
@@ -42,6 +47,13 @@ export default function Home() {
         <p>名前：{user.name}</p>
         <p>Eメール：{user.email}</p>
       </div>}
+      <button
+        type="submit"
+        onClick={Logout}
+        className="py-2 px-4 mt-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm md:text-base"
+      >
+        ログアウト
+      </button>
     </div>
   );
 }
