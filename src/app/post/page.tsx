6de0f932a596604f841d6756d0be9ef75140aab0
kappy0713 <s22253@g.ashikaga.ac.jp>
2024-06-17 14:59:24 +0900

    update：投稿画面UI

--- src/app/post/page.tsx
+++ src/app/post/page.tsx
@@ -30,52 +30,61 @@ export default function Page () {
   return (
     <div>
       <Header />
-      <h1 className="mt-20">投稿フォーム</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>タイトル:</label>
-          <input
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md mt-16">
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <div>
+                <label>タイトル</label>
+                <input
+                  type="text"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  className="border border-gray-300 rounded p-2 w-full"
+                />
+              </div>
+              <div>
+                <label>本文</label>
+                <textarea
+                  value={body}
+                  onChange={(e) => setBody(e.target.value)}
+                  className="border border-gray-300 rounded p-2 w-full resize-none h-32"
+                />
+              </div>
+              <div>
+                <label>日付：</label>
+                <input
+                  type="date"
+                  value={date.toISOString().split('T')[0]}
+                  onChange={(e) => setDate(new Date(e.target.value))}
+                />
+              </div>
+              <div>
+                <label>運動時間：</label>
+                <select value={hours} onChange={(e) => setHours(e.target.value)}>
+                  {[...Array(24)].map((_, i) => (
+                    <option key={i} value={i}>{i} 時間</option>
+                  ))}
+                </select>
+                <select value={minutes} onChange={(e) => setMinutes(e.target.value)}>
+                  {[...Array(12)].map((_, i) => (
+                    <option key={i} value={i * 5}>{i * 5} 分</option>
+                  ))}
+                </select>
+              </div>
+              <div className="flex justify-center">
+                <button
+                  onClick={Post}
+                  type="submit"
+                  className="py-2 px-4 mt-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm md:text-base"
+                >
+                  投稿
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
-        <div>
-          <label>本文:</label>
-          <textarea
-            value={body}
-            onChange={(e) => setBody(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>日付:</label>
-          <input
-            type="date"
-            value={date.toISOString().split('T')[0]}
-            onChange={(e) => setDate(new Date(e.target.value))}
-          />
-        </div>
-        <div>
-          <label>運動時間:</label>
-          <select value={hours} onChange={(e) => setHours(e.target.value)}>
-            {[...Array(24)].map((_, i) => (
-              <option key={i} value={i}>{i} 時間</option>
-            ))}
-          </select>
-          <select value={minutes} onChange={(e) => setMinutes(e.target.value)}>
-            {[...Array(12)].map((_, i) => (
-              <option key={i} value={i * 5}>{i * 5} 分</option>
-            ))}
-          </select>
-        </div>
-        <button
-          onClick={Post}
-          type="submit"
-          className="py-2 px-4 mt-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm md:text-base"
-        >
-          投稿
-        </button>
-      </form>
+      </div>
     </div>
   )
 }
--- src/app/share_post/page.tsx
+++ src/app/share_post/page.tsx
@@ -6,7 +6,6 @@ import { SharePost } from "@/utils/post";
 import Header from "@/app/Header"
 
 export default function Page () {
-  const today = new Date().toISOString().split('T')[0];
   const [title, setTitle] = useState('');
   const [body, setBody] = useState('');
   const [url, setURL] = useState('');
@@ -27,38 +26,48 @@ export default function Page () {
   return (
     <div>
       <Header />
-      <h1 className="mt-20">投稿フォーム</h1>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>タイトル:</label>
-          <input
-            type="text"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-          />
+      <div className="flex items-center justify-center min-h-screen bg-gray-100">
+        <div className="bg-white p-8 rounded-lg shadow-md w-full max-w-md mt-16">
+          <form onSubmit={handleSubmit}>
+            <div className="mb-4">
+              <div>
+                <label>タイトル</label>
+                <input
+                  type="text"
+                  value={title}
+                  onChange={(e) => setTitle(e.target.value)}
+                  className="border border-gray-300 rounded p-2 w-full"
+                />
+              </div>
+              <div>
+                <label>本文</label>
+                <textarea
+                  value={body}
+                  onChange={(e) => setBody(e.target.value)}
+                  className="border border-gray-300 rounded p-2 w-full resize-none h-32"
+                />
+              </div>
+              <div>
+                <label>URL</label>
+                <textarea
+                  value={url}
+                  onChange={(e) => setURL(e.target.value)}
+                  className="border border-gray-300 rounded p-2 w-full resize-none h-16"
+                />
+              </div>
+              <div className="flex justify-center">
+                <button
+                  onClick={Post}
+                  type="submit"
+                  className="py-2 px-4 mt-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm md:text-base"
+                >
+                  投稿
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
-        <div>
-          <label>本文:</label>
-          <textarea
-            value={body}
-            onChange={(e) => setBody(e.target.value)}
-          />
-        </div>
-        <div>
-          <label>URL:</label>
-          <textarea
-            value={url}
-            onChange={(e) => setURL(e.target.value)}
-          />
-        </div>
-        <button
-          onClick={Post}
-          type="submit"
-          className="py-2 px-4 mt-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:text-sm md:text-base"
-        >
-          投稿
-        </button>
-      </form>
+      </div>
     </div>
   )
 }
